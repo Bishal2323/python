f8cd080aac441846f3f6b90ef1a27ee71c94fa81
Bishal2323 <158798827+Bishal2323@users.noreply.github.com>
2024-12-20 18:21:26 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hey! How is your day going ?")
+response = input("Do you want to say anything to us .? (Y/N)")
+while response.upper() == "Y":
+    feedback=input("Are you happy or sad .?")
+
+    if feedback.upper() == "HAPPY":
+        print("You have a good one ")
+    if feedback.upper() == "SAD":
+        number = input (You have a my number you can call me.)
+    
+  
